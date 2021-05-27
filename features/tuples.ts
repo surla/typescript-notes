d6da477e9ef62f208fd91e7842150069001d7643
Joseph Surla <josephsurla@gmail.com>
2021-05-27 12:03:06 +0900

    Add tuples notes

---
+++ features/tuples.ts
@@ -0,0 +1,23 @@
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+// Tuple
+// const pepsi: [string, boolean, number] = ['brown', true, 40]
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+// Objects show more information than tuples
+const carStates = {
+    horsepower: 400,
+    weight: 3354
+}
